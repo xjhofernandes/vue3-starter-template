c529b3939aa624ba81d3a9fea098e7e0ea99a7d8
ericmiguel <emigr34@gmail.com>
2020-12-28 17:24:54 -0300

    First commit

--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
-createApp(App).mount('#app')
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faHeadset, faHome } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(faHeadset)
+library.add(faHome)
+
+require('@/assets/main.scss');
+
+const app = createApp(App)
+app.component('font-awesome-icon', FontAwesomeIcon)
+app.use(router)
+app.mount('#app')
---
+++ src/views/Home.vue
@@ -0,0 +1,33 @@
+<template>
+  <section class="hero is-primary is-fullheight">
+  
+    <HeroNavbar :routes="HeroNavbarRoutes" />
+
+    <div class="hero-head"></div>
+
+    <div class="hero-body">
+      <div class="container has-text-centered">
+        <h2 class="subtitle"><font-awesome-icon icon="home" /></h2>
+        <h1 class="title">Home</h1>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import HeroNavbar from "../components/HeroNavbar.vue";
+
+export default {
+  components: {
+    HeroNavbar,
+  },
+  data() {
+    return {
+      HeroNavbarRoutes: [
+        { "menu-name": "Home", "route-url": "/" },
+        { "menu-name": "Contact", "route-url": "/contact" },
+      ],
+    };
+  },
+};
+</script>
